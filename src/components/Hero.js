935fb70da8d27efb29dbf3f3b7554e19d3f741d7
Sachchaa <shewahal@deakin.edu.au>
2021-03-19 23:33:26 +1100

    Gatsby-travel-website updated -
    Started working on Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Hero = () => {
+    return (
+        <HeroContainer>
+            <HeroBackground>
+                <VideoBackground src='' type='video/mp4' />
+            </HeroBackground>
+        </HeroContainer>
+
+    )
+}
+
+export default Hero
+
+const HeroContainer = styled.div`
+
+`
+
+const HeroBackground = styled.div`
+
+`
+
+const VideoBackground = styled.video`
+
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 
 export default function Home() {
   return (
     <>
       <Layout >
+        <Hero />
 
       </Layout>
     </>
